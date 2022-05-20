03c69de4562fbd4910917ed1b5a4b5f73aa2f260
4txc7 <96539071+4txc7@users.noreply.github.com>
2022-05-20 16:27:58 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    token: process.env.TOKEN,
-    prefix: process.env.PREFIX
+    token: process.env.OTE2Mzc5OTU3MDEyMzQ0ODQz.G262sw.aymqlu3urGqcW7jVMIKlueNT6VlBM70PgI751Y,
+    prefix: process.env.!
 }
 // I have made all of this .envs!
